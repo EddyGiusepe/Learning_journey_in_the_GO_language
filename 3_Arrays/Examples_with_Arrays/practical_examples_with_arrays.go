de5f49a7c0e5f7edf851c6b3b594e21a18e18992
EddyGiusepe <eddychirinos.unac@gmail.com>
2025-09-06 15:04:55 -0300

    Examples em GO

--- 3_Arrays/Examples_with_Arrays/practical_examples_with_arrays.go
+++ 3_Arrays/Examples_with_Arrays/practical_examples_with_arrays.go
@@ -1,6 +1,9 @@
 // Senior Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
 
 /*
+Neste script vamos aprender como usar arrays em Go.
+Ademais, vamos usar o "package utils" para operações com arrays.
+
 RUN
 ---
 go run practical_examples_with_arrays.go
@@ -11,7 +14,10 @@ gofmt -w practical_examples_with_arrays.go
 */
 package main
 
-import "fmt"
+import (
+	"Learning_journey_in_the_GO_language/utils"
+	"fmt"
+)
 
 func main() {
 	// PASSO 8: EXEMPLOS PRÁTICOS
@@ -21,7 +27,7 @@ func main() {
 	// Exemplo 1: Encontrar o maior e menor valor em um array:
 	valores := [7]int{23, 45, 12, 67, 34, 89, 56}
 
-	maior, menor := encontrarMaiorMenor(valores)
+	maior, menor := utils.EncontrarMaiorMenor(valores)
 	fmt.Println("O array tem valores: ", valores)
 	fmt.Printf("O maior valor é: %d e o menor valor é: %d\n", maior, menor)
 
@@ -29,33 +35,7 @@ func main() {
 	temperaturas := [5]float64{23.5, 25.2, 22.8, 24.1, 26.3}
 
 	fmt.Println("\nOs valores das 5 temperaturas são:", temperaturas)
-	media_tem := calculaMedia(temperaturas)
+	media_tem := utils.CalculaMedia(temperaturas)
 	fmt.Printf("A média das temperaturas é: %.2f\n", media_tem)
 
-}
-
-// FUNÇÕES AUXILIARES
-// ===================
-
-func encontrarMaiorMenor(array [7]int) (int, int) {
-	maior := array[0]
-	menor := array[0]
-
-	for _, valor := range array {
-		if valor > maior {
-			maior = valor
-		}
-		if valor < menor {
-			menor = valor
-		}
-	}
-	return maior, menor
-}
-
-func calculaMedia(array [5]float64) float64 {
-	soma := 0.0
-	for _, valor := range array {
-		soma = soma + valor
-	}
-	return soma / float64(len(array))
 }
