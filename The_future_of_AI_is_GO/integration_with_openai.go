f55bbe349de7056a456eb88455e0f6da0a2a9be0
EddyGiusepe <eddychirinos.unac@gmail.com>
2025-09-12 01:11:42 -0300

    Usando OpenAI com GO

--- The_future_of_AI_is_GO/integration_with_openai.go
+++ The_future_of_AI_is_GO/integration_with_openai.go
@@ -13,19 +13,53 @@ go run integration_with_openai.go
 FORMATANDO O CÓDIGO
 -------------------
 gofmt -w integration_with_openai.go
+
+LEMBRANDO A INSTALAÇÃO DE DEPENDÊNCIAS
+--------------------------------------
+go get github.com/openai/openai-go/v2
+go get github.com/openai/openai-go/v2/option
+
+Pode usar o comando <go mod tidy> para limpar e reorganizar suas
+dependências.
 */
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/openai/openai-go/v2"
+	"github.com/openai/openai-go/v2/option"
 )
 
 func main() {
-	fmt.Println("Integrando a API da OpenAI com a linguagem Go!")
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("Erro ao carregar o arquivo .env")
+	}
+	OPENAI_API_KEY := os.Getenv("OPENAI_API_KEY")
 
-	client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
+	// Criando cliente OpenAI usando variável de ambiente
+	client := openai.NewClient(
+		option.WithAPIKey(OPENAI_API_KEY),
+	)
 	fmt.Printf("Cliente OpenAI criado com sucesso: %T\n", client)
+
+	// Fazendo uma requisição de chat completion
+	chatCompletion, err := client.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
+		Messages: []openai.ChatCompletionMessageParamUnion{
+			openai.UserMessage("Apenas diga: 'Olá Dr. Eddy Giusepe'"),
+		},
+		Model: openai.ChatModelGPT4o,
+	})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Imprimindo a resposta da IA
+	fmt.Println("Resposta da OpenAI:")
+	println(chatCompletion.Choices[0].Message.Content)
 }
